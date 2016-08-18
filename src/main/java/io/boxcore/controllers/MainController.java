da14b5221e1d4643f2aaa299ee5d3ff0c650f262
Craig Walker <Craig_Walker1@baylor.edu>
2016-08-18 00:47:34 -0500

    refactored project

---
+++ src/main/java/io/boxcore/controllers/MainController.java
@@ -0,0 +1,27 @@
+package io.boxcore.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * The Main controller.
+ * 
+ * @author Craig Walker
+ * @version 1.0
+ * @since 8/5/2016
+ *
+ */
+@Controller
+public class MainController {
+	
+	/**
+	 * Displays the home page.
+	 *
+	 * @return the UI home page
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+}
